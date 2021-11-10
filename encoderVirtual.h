c74fdc7b4c0c9ae4e2c4ec3a46fdb76fd2a9b7e5
Arthur <assistenciatecnica@sunnytec.com>
2021-11-10 15:48:14 -0300

    criou classe encoderVirtual.

--- encoderVirtual.h
+++ encoderVirtual.h
@@ -6,7 +6,16 @@
 class encoderVirtual
 {
 public:
+encoderVirtual(uint32_t _ppr, uint32_t _diametroDaPolia): ppr{_ppr}, diametroDaPolia{_diametroDaPolia}
+{
+
+}
+
 private:
+uint32_t ppr = 2000;
+uint32_t diametroDaPolia = 60; // mm
+
+
 };
 
 #endif
