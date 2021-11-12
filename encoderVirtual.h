30169567a1a50b6a5a732a204abe553d2587f28d
Arthur <assistenciatecnica@sunnytec.com>
2021-11-12 14:44:16 -0300

    mudei variaveis para float

--- encoderVirtual.h
+++ encoderVirtual.h
@@ -6,23 +6,40 @@
 class encoderVirtual
 {
 public:
-encoderVirtual(uint32_t _ppr, uint32_t _diametroDaPolia): ppr{_ppr}, diametroDaPolia{_diametroDaPolia}
-{
+    encoderVirtual(uint32_t _ppr, uint32_t _diametroDaPolia) : ppr{_ppr}, diametroDaPolia{_diametroDaPolia}
+    {
+    }
 
-}
+    void setVelocidade_metrosPorMinuto(uint32_t _velocidade)
+    {
+        velocidade_mmPorSegundo = _velocidade * 1000 / 60;
+        Serial.print("mm/s:");
+        Serial.println(velocidade_mmPorSegundo);
+        calculos();
+    }
 
-void setVelocidade_metrosPorMinuto(uint32_t _velocidade)
-{
-    velocidade_mmPorSegundo = _velocidade * 1000 / 60;
-}
+    float getPulsosPorSegundo()
+    {
+        return pulsosPorSegundo;
+    }
 
 private:
-uint32_t ppr = 2000; // pulsos por rotação
-uint32_t diametroDaPolia = 60; // mm
-uint32_t velocidade_mmPorSegundo = 200; //mm/s
-uint32_t pulsosPorMm = round(ppr/((float)diametroDaPolia * 3.14));
-uint32_t pulsosPorSegundo = pulsosPorMm * velocidade_mmPorSegundo;
+    uint32_t ppr = 2000;                    // pulsos por rotação
+    uint32_t diametroDaPolia = 60;          // mm
+    float velocidade_mmPorSegundo = 200; //mm/s
+    float pulsosPorMm = ppr / ((float)diametroDaPolia * 3.14159);
+    float pulsosPorSegundo = pulsosPorMm * velocidade_mmPorSegundo;
 
+    void calculos()
+    {
+        pulsosPorMm = ppr / ((float)diametroDaPolia * 3.14159);
+        pulsosPorSegundo = pulsosPorMm * velocidade_mmPorSegundo;
+
+        Serial.print("p/mm:");
+        Serial.println(pulsosPorMm);
+        Serial.print("p/s:");
+        Serial.println(pulsosPorSegundo);
+    }
 };
 
 #endif
