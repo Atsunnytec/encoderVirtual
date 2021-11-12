ff9187f7e76e20ee9923e3f1e83d6d1dd3266e23
Arthur <assistenciatecnica@sunnytec.com>
2021-11-12 15:59:18 -0300

    passei funcoes de timer para o arquivo do encoder virtual.

--- encoderVirtual.h
+++ encoderVirtual.h
@@ -3,11 +3,22 @@
 
 #include <Arduino.h>
 
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+volatile int interruptCounter;
+
+void IRAM_ATTR onTimer()
+{
+    portENTER_CRITICAL_ISR(&timerMux);
+    interruptCounter++;
+    portEXIT_CRITICAL_ISR(&timerMux);
+}
+
 class encoderVirtual
 {
 public:
     encoderVirtual(uint32_t _ppr, uint32_t _diametroDaPolia) : ppr{_ppr}, diametroDaPolia{_diametroDaPolia}
     {
+        configureTimer();
     }
 
     void setVelocidade_metrosPorMinuto(uint32_t _velocidade)
@@ -23,22 +34,51 @@ public:
         return pulsosPorSegundo;
     }
 
+    uint32_t getCounter()
+    {
+        return interruptCounter;
+    }
+
+    void setCounter(uint32_t value)
+    {
+        portENTER_CRITICAL(&timerMux);
+        interruptCounter = value;
+        portEXIT_CRITICAL(&timerMux);
+    }
+
 private:
-    uint32_t ppr = 2000;                    // pulsos por rotação
-    uint32_t diametroDaPolia = 60;          // mm
+    uint32_t ppr = 2000;                 // pulsos por rotação
+    uint32_t diametroDaPolia = 60;       // mm
     float velocidade_mmPorSegundo = 200; //mm/s
     float pulsosPorMm = ppr / ((float)diametroDaPolia * 3.14159);
     float pulsosPorSegundo = pulsosPorMm * velocidade_mmPorSegundo;
+
+    uint32_t usPorPulso = 471;
+
+    hw_timer_t *timer = NULL;
+
+    const int16_t prescaler = 80;
 
     void calculos()
     {
         pulsosPorMm = ppr / ((float)diametroDaPolia * 3.14159);
         pulsosPorSegundo = pulsosPorMm * velocidade_mmPorSegundo;
+        usPorPulso = round((float)1000000 / pulsosPorSegundo);
 
+        Serial.print("us/p:");
+        Serial.println(usPorPulso);
         Serial.print("p/mm:");
         Serial.println(pulsosPorMm);
         Serial.print("p/s:");
         Serial.println(pulsosPorSegundo);
+    }
+
+    void configureTimer()
+    {
+        timer = timerBegin(0, prescaler, true);
+        timerAttachInterrupt(timer, onTimer, true);
+        timerAlarmWrite(timer, usPorPulso, true);
+        timerAlarmEnable(timer);
     }
 };
 
